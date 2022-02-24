c654be72559972171f2e03904e35b61c8be3eff1
Ebenezer Hailemariam <eben@derso.org>
2022-02-24 12:06:12 -0500

    Added test

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 import unittest
-from rain import get_rain_data
+from rain import get_rain_data, get_lat_lon
 
 
 class Tests(unittest.TestCase):
@@ -10,3 +10,9 @@ class Tests(unittest.TestCase):
 
     def test_get_rain_data_nonexistent_file(self):
         self.assertRaises(FileNotFoundError, get_rain_data, '/home/eben/foo.csv')
+
+    def test_get_lat_lon_valid_city(self):
+        self.assertEqual(get_lat_lon('Toronto'), ('43.6534817', '-79.3839347'))
+
+    def test_get_lat_lon_invalid_city(self):
+        self.assertRaises(ValueError, get_lat_lon, 'FooBazBar')
