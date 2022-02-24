97b6336635139f91908bbf57fa555da009cf7311
Ebenezer Hailemariam <eben@derso.org>
2022-02-24 11:30:57 -0500

    Added test case

--- tests/test.py
+++ tests/test.py
@@ -3,5 +3,8 @@ from rain import get_rain_data
 
 
 class TestNumbers(unittest.TestCase):
-    def test_get_rain_data(self):
-        self.assertTrue(True)
+    def test_get_rain_data_default_file(self):
+        rain_data = get_rain_data()
+        self.assertEqual(len(rain_data), 1162584)
+        self.assertEqual(len(rain_data[0]), 4)
+
