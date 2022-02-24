a1844addca1c8228a5e5713013ca7b7bcf7869c4
Ebenezer Hailemariam <eben@derso.org>
2022-02-24 11:39:20 -0500

    Gave test class somewhat more sensible name

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import unittest
 from rain import get_rain_data
 
 
-class TestNumbers(unittest.TestCase):
+class Tests(unittest.TestCase):
     def test_get_rain_data_default_file(self):
         rain_data = get_rain_data()
         self.assertEqual(len(rain_data), 1162584)
